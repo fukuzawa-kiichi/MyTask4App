5692718759fe516fc5c756db1684ee1109c2eedb
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-07 15:39:30 +0900

    firstcommit

---
+++ MyTask4App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTask4App
+//
+//  Created by VERTEX24 on 2019/08/07.
+//  Copyright © 2019 VERTEX24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
